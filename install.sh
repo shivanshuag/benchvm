06b6d59678a120a7f36cf050d5de76a3205fe4b0
katrakc <katrakc@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-07 19:29:56 +0000

    changed install scripts

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#For creator script:
-apt-get install -y debootstrap multipath-tools
-#KVM
-apt-get install kvm qemu bridge-utils uml-utilities
-#Xen
-apt-get install bridge-utils libxen3.1 python-xen-3.1 xen-docs-3.1 xen-hypervisor-3.1 xen-ioemu-3.1 xen-tools xen-utils-3.1 linux-xen
-cp ./dom0_config/interfaces /etc/network/interfaces
