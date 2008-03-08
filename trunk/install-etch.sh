952ea0077bc232c4056e9bf279d6c1111673ad71
katrakc <katrakc@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-08 02:11:12 +0000

    Added a bit of starter-kvm, fixed bug in creator, updated etch install.

--- trunk/install-etch.sh
+++ trunk/install-etch.sh
@@ -4,10 +4,9 @@ cp etch_dom0/70debconf /etc/apt/apt.conf.d/
 cp etch_dom0/sources.list /etc/apt/
 apt-get update
 #For creator script:
-apt-get install -y debootstrap multipath-tools
+apt-get install -y debootstrap multipath-tools parted
 #KVM
 apt-get install -y kvm kvm-source qemu bridge-utils uml-utilities
 m-a a-i kvm
 #Xen
-apt-get install -y linux-image-2.6-xen-amd64 xen-utils-common 
-
+apt-get install -y linux-image-2.6-xen-amd64 xen-utils-common xen-hypervisor-3.2-1-amd64 xen-utils-3.2-1
