e593ced3f4c314b9708bb22b3eac4b2bcd7a1f7a
eightball1989 <eightball1989@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-08 20:50:35 +0000

    added kvm-source and m-a a-i process

--- install-gutsy-32.sh
+++ install-gutsy-32.sh
@@ -7,7 +7,8 @@ apt-get install -y emacs22-nox vim1
 #For creator script:
 apt-get install -y debootstrap multipath-tools
 #KVM
-apt-get install -y kvm qemu bridge-utils uml-utilities
+apt-get install -y kvm kvm-source module-assistant qemu bridge-utils uml-utilities
+m-a a-i kvm
 #Xen
 #apt-get install -y libxen3.1 python-xen-3.1 xen-docs-3.1 xen-hypervisor-3.1 xen-ioemu-3.1 xen-tools xen-utils-3.1 linux-xen
 cp ./dom0/interfaces /etc/network/interfaces
