f8b090f56dfa92cf5b818dd1538d4a1053e6016a
eightball1989 <eightball1989@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-08 22:28:52 +0000

    Script changes

--- install-hardy-64.sh
+++ install-hardy-64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Configure Apt
-cp gutsy-32_dom0/sources.list /etc/apt/
+cp hardy-64_dom0/sources.list /etc/apt/
 apt-get update
 #noessential
 apt-get install -y emacs22-nox vim
@@ -10,7 +10,7 @@ apt-get install -y debootstrap multipath-tools
 apt-get install -y kvm kvm-source module-assistant qemu bridge-utils uml-utilities
 m-a a-i kvm
 #Xen
-apt-get install -y libxen3.1 python-xen-3.1 xen-docs-3.1 xen-hypervisor-3.1 xen-ioemu-3.1 xen-tools xen-utils-3.1 linux-xen
+
 cp ./dom0/interfaces /etc/network/interfaces
 /etc/init.d/networking restart
 ./start_kvm.sh
