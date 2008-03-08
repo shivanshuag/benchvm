481e9e3d0cecf9ed40ae25656166eae698e3750c
eightball1989 <eightball1989@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-08 20:29:44 +0000

    restart networking

--- install-gutsy-32.sh
+++ install-gutsy-32.sh
@@ -11,3 +11,4 @@ apt-get install -y kvm qemu bridge-utils uml-utilities
 #Xen
 #apt-get install -y libxen3.1 python-xen-3.1 xen-docs-3.1 xen-hypervisor-3.1 xen-ioemu-3.1 xen-tools xen-utils-3.1 linux-xen
 cp ./dom0/interfaces /etc/network/interfaces
+/etc/init.d/networking restart
