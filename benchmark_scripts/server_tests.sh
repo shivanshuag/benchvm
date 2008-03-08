5d495bde4d74ac5ccd0ab15a170a160e0a7fe18d
martman00 <martman00@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-08 18:48:33 +0000

    added iperf script, still needs fixing

--- benchmark_scripts/server_tests.sh
+++ benchmark_scripts/server_tests.sh
@@ -2,6 +2,9 @@
 
 echo "Starting Webserver Tests"
 echo "========================"
-bash netperf_test.sh $1
 bash ab_test.sh $1
 bash httperf_test.sh $1
+
+echo "Starting Networking tests"
+echo "========================="
+bash netperf_remote_test.sh $1
