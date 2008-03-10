b3ff85fb841bf774f113d3582195ec075d382001
katrakc <katrakc@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-10 05:06:04 +0000

    fixed output

--- trunk/benchmark_scripts/iozone_test.sh
+++ trunk/benchmark_scripts/iozone_test.sh
@@ -7,15 +7,15 @@ echo "Running iozone tests"
 echo "======================"
 
 echo -n "* Starting default test..."
-iozone -Rab $1/iozone_results/default.wks
+iozone -Rab $loc/iozone_results/default.wks
 echo "Done"
 
 echo -n "* Starting random async test..."
-iozone -RabK $1/iozone_results/default.wks
+iozone -RabK $loc/iozone_results/default.wks
 echo "Done"
 
 echo -n "* Starting random sync test..."
-iozone -RabKo $1/iozone_results/default.wks
+iozone -RabKo $loc/iozone_results/default.wks
 echo "Done"
 
 echo "Finished Tests
