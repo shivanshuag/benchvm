1842314fea0f9df9793f52191b2bf1cf0858f70e
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-11 03:00:29 +0000

    fixed unixbench

--- benchmark_scripts/unixbench_test.sh
+++ benchmark_scripts/unixbench_test.sh
@@ -3,14 +3,18 @@
 loc=~/unixbench_results
 mkdir -p $loc 
 
+cd ~/unixbench-4.1.0
+make
+
 echo "Starting unixbench Tests"
 echo "========================"
 
 for num_tests in 1 2
 do
 	echo -n "* Starting test($num_tests)..."
-	~/unixbench-4.1.0/Run &> $loc/$num_tests.output
+	./Run &> $loc/$num_tests.output
 	echo "Done"
 done
 
 echo "Finished Tests"
+cd ~
