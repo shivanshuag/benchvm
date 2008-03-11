a1624fbb1a6cc784faa4319be7ac22236fc151eb
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-11 02:50:45 +0000

    fixed filename bug

--- benchmark_scripts/iozone_test.sh
+++ benchmark_scripts/iozone_test.sh
@@ -11,11 +11,11 @@ iozone -Rab $loc/iozone_results/default.wks
 echo "Done"
 
 echo -n "* Starting random async test..."
-iozone -RabK $loc/iozone_results/default.wks
+iozone -RaKb $loc/iozone_results/random.async.wks
 echo "Done"
 
 echo -n "* Starting random sync test..."
-iozone -RabKo $loc/iozone_results/default.wks
+iozone -RaKob $loc/iozone_results/random.sync.wks
 echo "Done"
 
 echo "Finished Tests"
