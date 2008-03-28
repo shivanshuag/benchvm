05ed4944a964d5385fac615db5f185099b7ae436
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-28 23:27:05 +0000

    add stress tests to get_workload.sh and extract during guest creation

--- trunk/get_workloads.sh
+++ trunk/get_workloads.sh
@@ -4,3 +4,4 @@ wget http://mirror.clarkson.edu/benchvm/workload.tar.gz
 wget http://www.tux.org/pub/tux/niemi/unixbench/unixbench-4.1.0.tgz
 wget http://www.kernel.org/pub/linux/kernel/people/ck/apps/kernbench/kernbench-0.42.tar.gz
 wget http://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.24.3.tar.bz2
+wget http://mirror.clarkson.edu/benchvm/stresstest-030708.tar
