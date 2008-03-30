f4ddf3ee3c5e291ddf7394a27b0acf47140aab26
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-03-30 17:46:39 +0000

    fixed upload results script

--- trunk/bin/upload_new_results.sh
+++ trunk/bin/upload_new_results.sh
@@ -7,8 +7,10 @@ if [ -z "$1" ]
 fi
 echo "uploading $1 stress test results"
 
-cp /opt/SPECweb2005/results/* /root/benchvm/isolation-results/
+my_ip=`ifconfig  | grep 192 | awk -F: '{print $2}' | awk  '{print $1}'`
+
+cp /opt/SPECweb2005/results/* /root/benchvm/isolation-results/${my_ip}/
 mv /opt/SPECweb2005/results/* /opt/SPECweb2005/results.bak/
-cd /root/benchvm/isolation-results
+cd /root/benchvm/isolation-results/${my_ip}
 svn add *
 svn commit -m " ${1} stress test results"
