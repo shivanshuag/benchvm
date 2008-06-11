e9ae80b77b7e19857972cfada56af5434e507f28
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-06-11 05:23:31 +0000

    add some useful scripts back to trunk

---
+++ clean_isolation_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f workload.tar.gz
---
+++ get_isolation_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://mirror.clarkson.edu/benchvm/workload.tar.gz
+wget http://mirror.clarkson.edu/benchvm/stresstest-030708.tar
