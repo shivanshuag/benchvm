c91c78ebe1b47c7be4e3ef2acaa66d578d61865f
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-06-13 17:12:42 +0000

    fix and make live upload script

--- upload_new_results.sh
+++ upload_new_results.sh
@@ -10,8 +10,8 @@ echo "uploading $1 $2 $3 test results"
 #my_ip=`ifconfig  | grep 192 | awk -F: '{print $2}' | awk  '{print $1}'`
 
 host=`hostname`
-echo "cp /opt/SPECweb2005/results/* /root/benchvm/results/$2/$1/$3/${host}/"
-echo "mv /opt/SPECweb2005/results/* /opt/SPECweb2005/results.xen-summit/"
-echo "cd /root/benchvm/results/$2/$1/$3/${host}/"
-echo 'svn add *'
-echo "svn commit -m \"add ${1} ${2} ${3} test results\""
+cp /opt/SPECweb2005/results/* /root/devel/benchvm/results/$2/$1/$3/${host}/
+mv /opt/SPECweb2005/results/* /opt/SPECweb2005/results.xen-summit/
+cd /root/benchvm/results/$2/$1/$3/${host}/"
+svn add *
+svn commit -m "add ${1} ${2} ${3} test results from ${host}"
