9937550d03314c19d584898e4ab01298d19d2220
deshantm <deshantm@d7ab9b66-6d47-0410-8209-af629339f7ac>
2008-09-06 22:19:00 +0000

    update to phoronix test suite 1.2.0 and fix dependencies

--- bin/get_phoronix.sh
+++ bin/get_phoronix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-wget http://www.phoronix-test-suite.com/releases/deb/phoronix-test-suite_1.0.0_all.deb
+if [ ! -e phoronix-test-suite_1.2.0_all.deb ]
+  then
+  wget http://www.phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_1.2.0_all.deb
+fi
--- bin/get_workload.sh
+++ bin/get_workload.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-wget http://mirror.clarkson.edu/benchvm/workload.tar.gz
-wget http://mirror.clarkson.edu/benchvm/stresstest-030708.tar
+
+if [ ! -e workload.tar.gz ]
+  then
+  wget http://mirror.clarkson.edu/benchvm/workload.tar.gz
+fi
